643e8d8e511678584bafad770f38b25e03c9d511
Kim Emmanuel <kim.emmanuel@gmail.com>
2023-06-20 19:44:32 -0300

    feat: can edit other mechanics

--- src/lib/components/character/OtherMechanics.svelte
+++ src/lib/components/character/OtherMechanics.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-    import { character } from '../../stores/currentCharacter'
+    import characterRepository from '../../stores/characterRepository';
+    import Editable from '../shared/Editable.svelte'
 </script>
 
-{#each $character.mechanics.other as mechanic}
+{#each $characterRepository.current.mechanics.other as mechanic, index}
     <div class="flex justify-between items-center p-1">
-        <span>
-            {mechanic.name}
-        </span>
+        <Editable>
+            <input slot=editing id={`mechanic-${index}-name`} type="text" class="input w-5/12" bind:value={mechanic.name}>
+            <span slot=showing>{mechanic.name}</span>
+        </Editable>
 
-        <span class="text-2xl">
-            {mechanic.value}
-        </span>
+        <Editable>
+            <input slot=editing id={`mechanic-${index}-value`} type="text" class="input w-5/12" bind:value={mechanic.value}>
+            <span slot=showing class="text-2xl">{mechanic.value}</span>
+        </Editable>
     </div>
 {/each}
