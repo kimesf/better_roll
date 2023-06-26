72a35e91864bf8a0dcf62d7baf3801682ea8a33a
Kim Emmanuel <kim.emmanuel@gmail.com>
2023-06-26 11:10:56 -0300

    feat: can create and destroy mechanic

--- src/lib/components/character/OtherMechanics.svelte
+++ src/lib/components/character/OtherMechanics.svelte
@@ -1,7 +1,43 @@
 <script lang="ts">
     import characterRepository from '../../stores/characterRepository'
+    import { t } from '../../stores/i18n'
     import Editable from '../shared/Editable.svelte'
+    import Separator from '../shared/Separator.svelte'
+    import Title from '../shared/Title.svelte'
+    import BtnAction from '../shared/BtnAction.svelte'
+    import type { Mechanic } from '../../types'
+
+    const DEFAULT: Mechanic = {
+        name: '',
+        value: ''
+    }
+
+    const newMechanic = (): Mechanic => {
+        return structuredClone(DEFAULT)
+    }
+
+    const trigger = (): void => {
+        $characterRepository.current.mechanics.other = $characterRepository.current.mechanics.other
+    }
+
+    const create = (): void => {
+        $characterRepository.current.mechanics.other.push(newMechanic())
+        trigger()
+    }
+
+    const destroy = (index: number): void => {
+        $characterRepository.current.mechanics.other.splice(index, 1)
+        trigger()
+    }
 </script>
+
+<Title title={t('display.mechanics')} />
+
+<Separator />
+
+<Editable>
+    <BtnAction kind=create class="mt-2 w-full" handler={(_e) => create()}>{t('actions.create')}</BtnAction>
+</Editable>
 
 {#each $characterRepository.current.mechanics.other as mechanic, index}
     <div class="flex justify-between items-center p-1">
@@ -21,10 +57,14 @@
                 slot="editing"
                 id={`mechanic-${index}-value`}
                 type="text"
-                class="input w-5/12"
+                class="input w-5/12 ml-2"
                 bind:value={mechanic.value}
             />
             <span slot="showing" class="text-2xl">{mechanic.value}</span>
+        </Editable>
+
+        <Editable>
+            <BtnAction kind=destroy class="w-16 ml-2" handler={(_e) => destroy(index)}>r</BtnAction>
         </Editable>
     </div>
 {/each}
