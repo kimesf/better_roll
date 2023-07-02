9910f8cdb79c18cae52ebaa11518076305019fcb
Kim Emmanuel <kim.emmanuel@gmail.com>
2023-07-02 04:20:14 -0300

    bug: characters menu btn not showing

--- src/lib/components/Display.svelte
+++ src/lib/components/Display.svelte
@@ -41,7 +41,7 @@
 <div>
     {#if menuVisible}
         <div class="z-40 bottom-0 fixed h-screen w-screen flex flex-col justify-between">
-            <button on:click={() => characterRepository.select(null) } class="p-4 text-2xl w-full bg-secondary">
+            <button on:click={() => characterRepository.select(null) } class="fixed top-0 p-4 text-2xl w-full bg-secondary">
                 {t('display.characters')}
             </button>
 
