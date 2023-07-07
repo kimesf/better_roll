e15840c6969c202dd990ba014b6f8a18682aaed8
Kim Emmanuel <kim.emmanuel@gmail.com>
2023-07-07 10:33:19 -0300

    feat: PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [
+        svelte(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png}'],
+            },
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Better Roll',
+                short_name: 'Better Roll',
+                description:
+                    'Better Roll is a simple and straight forward D&D character sheet manager designed for mobile',
+                theme_color: '#171717',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+            },
+        }),
+    ],
     test: {
         globals: true,
         environment: 'happy-dom',
